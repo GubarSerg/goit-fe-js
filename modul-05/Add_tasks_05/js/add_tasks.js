e81704840495845666433a9d1009efbbbbd772bd
GubarSerg <gubar.serg@gmail.com>
2018-12-08 19:55:00 +0200

    )

--- modul-05/Add_tasks_05/js/add_tasks.js
+++ modul-05/Add_tasks_05/js/add_tasks.js
@@ -97,16 +97,241 @@
   PS: обязательно используйте функциональные методы массивов, никаких for!
 */
 
-const guests = [
-  { name: "Mango", age: 20, isActive: true },
-  { name: "Poly", age: 18, isActive: false },
-  { name: "Ajax", age: 30, isActive: true },
-  { name: "Chelsey", age: 45, isActive: false }
-];
-const getPropValues = (arr, prop) => arr.map(prop => prop.name);
+// const guests = [
+//   { name: "Mango", age: 20, isActive: true },
+//   { name: "Poly", age: 18, isActive: false },
+//   { name: "Ajax", age: 30, isActive: true },
+//   { name: "Chelsey", age: 45, isActive: false }
+// ];
+
+// const getPropValues = (arr, prop) => arr.map(function(user) {
+//   if(user.hasOwnProperty(prop)) {
+//     return user[prop]
+//   }
+// })
+// // Вызовы функции для проверки
+// console.log( getPropValues(guests, "name") ); // ['Mango', 'Poly', 'Ajax', 'Chelsey']
+
+// console.log( getPropValues(guests, "age") ); // [20, 18, 30, 45]
+
+// console.log( getPropValues(guests, "isActive") ); // [true, false, true, false]
+
+
+// TASK 03
+
+/*      
+  Напишите функцию setGuestState(guests, period), где
+  guests - массив гостей, period - кол-во дней после
+  которого считается что гость не активен.
+    
+  Если значение поля inactiveDays болше чем period, 
+  поставить для isActive значение false.
+    
+  Если же значение inactiveDays меньше чем period,
+  поставить для isActive значение true
+  
+  PS: обязательно используйте функциональные методы массивов, никаких for!
+*/
+
+// const guests = [
+//   { name: 'Mango', inactiveDays: 15, isActive: true },
+//   { name: 'Poly', inactiveDays: 8, isActive: false },
+//   { name: 'Ajax', inactiveDays: 32, isActive: false },
+//   { name: 'Chelsey', inactiveDays: 85, isActive: true }
+// ];
+
+// const setGuestState = (users, period) => users.map(user => ({
+//   ...user,
+//   isActive: user.inactiveDays < period
+// }))
+
+// // Вызовы функции для проверки
+// console.log(
+//   setGuestState(guests, 10)
+// ); // Объекты Mango, Ajax, Chelsey получат isActive false, а Poly наоборот true
+
+// console.log(
+//   setGuestState(guests, 20)
+// ); // Объекты Ajax, Chelsey получат isActive false, а Mango и Poly наоборот true
+
+// console.log(
+//   setGuestState(guests, 50)
+// ); // Объект Chelsey получит isActive false, а Mango, Poly и Ajax наоборот true
+
+// TASK 04
+
+/*
+  Напишите функию getActiveGuests(guests), принимающую 
+  один параметр guests - массив объектов гостей. 
+  
+  Функция должна возвращать массив объектов гостей,
+  значение поля isActive которых true.
+         
+  PS: обязательно используйте функциональные методы массивов, никаких for!
+*/
+
+// const guests = [
+//   { name: "Mango", age: 20, isActive: true },
+//   { name: "Poly", age: 18, isActive: false },
+//   { name: "Ajax", age: 30, isActive: true },
+//   { name: "Chelsey", age: 45, isActive: false }
+// ];
+
+// const getActiveGuests = guests => guests.filter(user => user.isActive)
+
+// // Вызовы функции для проверки
+// console.log(getActiveGuests(guests)); // массив из 2-х объектов Mango и Ajax
+
+// TASK 05
+
+/*      
+  Напишите функцию getGuestsOlderThan(guests, age), где 
+  guests - массив объектов гостей, age - предел возраста 
+  для сортировки. 
+  
+  Функция возвращает массив объектов значение свойства 
+  age которых больше чем параметр age.
+  
+  PS: обязательно используйте функциональные методы массивов, никаких for!
+*/
+
+// const guests = [
+//   { name: "Mango", age: 20, isActive: true },
+//   { name: "Poly", age: 18, isActive: false },
+//   { name: "Ajax", age: 30, isActive: true },
+//   { name: "Chelsey", age: 45, isActive: false }
+// ];
+
+// const getGuestsOlderThan = (guests, age) => guests.filter(user => user.age > age)
+
+// // Вызовы функции для проверки
+// console.log(getGuestsOlderThan(guests, 25)); // массив из 2-х объектов Ajax и Chelsey
+
+// console.log(getGuestsOlderThan(guests, 35)); // [{name: 'Chelsey', age: 45, isActive: false}]
+
+// console.log(getGuestsOlderThan(guests, 55)); // []
+
+// TASK 06
+
+/*
+  Напишите функию getGuestById(guests, id), принимающую 
+  guests - массив объектов гостей, id - идентификатор (число). 
+  
+  Функция должна возвращать объект гостя с совпадающим id.
+  
+  PS: обязательно используйте функциональные методы массивов, никаких for!
+*/
+
+// const guests = [
+//   { id: 1, name: 'Mango', age: 20 },
+//   { id: 2, name: 'Poly', age: 18 },
+//   { id: 3, name: 'Ajax', age: 30 },
+//   { id: 4, name: 'Chelsey', age: 45 }
+// ];
+
+// const getGuestById = (guests, id) => guests.find(user => user.id === id)
+
+// // Вызовы функции для проверки
+// console.log(
+//   getGuestById(guests, 3)
+// ); // {id: 3, name: 'Ajax', age: 30}
+
+// console.log(
+//   getGuestById(guests, 1)
+// ); // {id: 1, name: 'Mango', age: 20}
+
+// console.log(
+//   getGuestById(guests, 5)
+// ); // undefined
+
+// TASK 07
+
+/*
+  Используя метод reduce, посчитайте сумму 
+  всех значений свойств объекта order.
+*/ 
+// const order = {
+//   bread: 10,
+//   apples: 25,
+//   chicken: 60,
+//   milk: 15,
+//   cheese: 40
+// };
+
+// const getSummOfValues = Object.values(order).reduce((acc, value) => acc + value, 0)
+
+// console.log(getSummOfValues); // 150
+
+// TASK 08
+
+/*
+  Напишите функцию getTotalPrice(products, order), где 
+  products - объект со свойствами "имя продукта":"цена за единицу"
+  order - объект со свойствами "имя продукта":"количество единиц".
+  
+  Функция возвращает общую сумму стоимости всех продуктов заказа.
+  
+  PS: используйте метод reduce
+*/
+
+const products = {
+  bread: 10,
+  milk: 15,
+  apples: 20,
+  cheese: 30,
+  chicken: 40
+};
+
+const orderA = {
+  bread: 2,
+  apples: 4,
+  chicken: 1
+};
+
+const orderB = {
+  bread: 1,
+  milk: 2,
+  cheese: 3
+};
+
+
+const getTotalPrice = (products, order) => ??????
+
 // Вызовы функции для проверки
-console.log( getPropValues(guests, "name") ); // ['Mango', 'Poly', 'Ajax', 'Chelsey']
 
-console.log( getPropValues(guests, "age") ); // [20, 18, 30, 45]
+console.log(getTotalPrice(products, orderA)); // 140
 
-console.log( getPropValues(guests, "isActive") ); // [true, false, true, false]
+console.log(getTotalPrice(products, orderB)); // 130
+
+
+
+// TASK 09
+
+/*     
+  Напишите функию allGuestsActive(guests), принимающую 
+  один параметр guests - массив объектов гостей. 
+  
+  Функция должна возвращать true если значение поля isActive 
+  всех объектов true, в противном случае false.
+  
+  PS: используйте метод every или some, никаких for!
+*/
+
+// const guestsA = [
+//   { name: "Mango", isActive: true },
+//   { name: "Poly", isActive: false },
+//   { name: "Ajax", isActive: true }
+// ];
+
+// const guestsB = [
+//   { name: "Mango", isActive: true },
+//   { name: "Poly", isActive: true },
+//   { name: "Ajax", isActive: true }
+// ];
+
+// const allGuestsActive = guests => guests.every(user => user.isActive)
+
+// // Вызовы функции для проверки
+// console.log(allGuestsActive(guestsA)); // false
+
+// console.log(allGuestsActive(guestsB)); // true
