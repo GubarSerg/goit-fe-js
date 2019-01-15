4850fe9b8a33547c1d911433b6843ee7c6047cf2
GubarSerg <gubar.serg@gmail.com>
2019-01-15 15:49:00 +0200

    added task5

--- modul-06/Add_tasks_06/js/add_tasks.js
+++ modul-06/Add_tasks_06/js/add_tasks.js
@@ -87,10 +87,10 @@
 // stringBuilder.pad("!!!");
 // stringBuilder.showValue(); // '=^.^='
 
-//  TASK 03
+// TASK 03
 
 // /*
-//   Создайте класс Car с указанными полями и методами.
+//  Создайте класс Car с указанными полями и методами.
 // */
 
 // class Car {
@@ -98,104 +98,182 @@
 // .....
 // }
 
-class Car {
-  constructor(maxSpeed) {
-    // - speed - для отслеживания текущей скорости, изначально 0.
-    this.speed = 0;
-    // - maxSpeed - для хранения максимальной скорости
-    this.maxSpeed = maxSpeed;
-    // - running - для отслеживания заведен ли автомобиль, возможные значения true или false. Изначально false.
-    this.running = false;
-    // - distance - содержит общий киллометраж, изначально с 0
-    this.distance = 0;
-  }
+// class Car {
+//   constructor(maxSpeed) {
+//     // - speed - для отслеживания текущей скорости, изначально 0.
+//     this.speed = 0;
+//     // - maxSpeed - для хранения максимальной скорости
+//     this.maxSpeed = maxSpeed;
+//     // - running - для отслеживания заведен ли автомобиль, возможные значения true или false. Изначально false.
+//     this.running = false;
+//     // - distance - содержит общий киллометраж, изначально с 0
+//     this.distance = 0;
+//   }
 
-  turnOn() {
-    // Добавьте код для того чтобы завести автомобиль, просто записывает в свойство running значание true
-    this.running = true;
-  }
+//   turnOn() {
+//     // Добавьте код для того чтобы завести автомобиль, просто записывает в свойство running значание true
+//     this.running = true;
+//   }
 
-  turnOff() {
-    // Добавьте код для того чтобы заглушить автомобиль, просто записывает в свойство running значание false
-    this.running = false;
-  }
+//   turnOff() {
+//     // Добавьте код для того чтобы заглушить автомобиль, просто записывает в свойство running значание false
+//     this.running = false;
+//   }
 
-  accelerate(spd) {
-    // Записывает в поле speed полученное значение, при условии что оно не больше чем значение свойства maxSpeed
-    if ((spd < this.maxSpeed) && (spd > this.speed)) {
-      this.speed = spd;
-    }
-    return this.speed;
-  }
+//   accelerate(spd) {
+//     // Записывает в поле speed полученное значение, при условии что оно не больше чем значение свойства maxSpeed
+//     if (spd < this.maxSpeed) {
+//       this.speed = spd;
+//     }
+//     return this.speed;
+//   }
 
-  decelerate(spd) {
-    // Записывает в поле speed полученное значение, при условии что оно не больше чем значение свойства maxSpeed и не меньше нуля
-    if ((spd > 0) && (spd <= this.speed)) {
-      this.speed = spd;
-    }
-    return this.speed;
-  }
+//   decelerate(spd) {
+//     // Записывает в поле speed полученное значение, при условии что оно не больше чем значение свойства maxSpeed и не меньше нуля
+//     if (0 <= spd < this.maxSpeed) {
+//       this.speed = spd;
+//     }
+//     return this.speed;
+//   }
 
-  drive(hours) {
-    // Добавляет в поле distance киллометраж (hours умноженное на значение поля speed), но только в том случае если машина заведена!
-    if (this.turnOn) {
-      this.distance = this.maxSpeed * hours;
-    }
-  }
+//   drive(hours) {
+//     // Добавляет в поле distance киллометраж (hours умноженное на значение поля speed), но только в том случае если машина заведена!
+//     if (this.turnOn) {
+//       this.distance = this.maxSpeed * hours;
+//     }
+//   }
 
-  static getSpecs(car) {
-    console.log(
-      `maxSpeed: ${car.maxSpeed}, running: ${car.running}, distance: ${
-        car.distance
-      }`
-    );
-  }
-}
+//   static getSpecs(car) {
+//     console.log(
+//       `maxSpeed: ${car.maxSpeed}, running: ${car.running}, distance: ${
+//         car.distance
+//       }`
+//     );
+//   }
+// }
 
-// TASK 04
+// // TASK 04
 
-// /*
-//   Добавьте к классу Car из предыдущего задания статический
-//   метод getSpecs, который получает объект-машину как аргумент
-//   и выводит в консоль значения полей maxSpeed, running и distance.
+// // /*
+// //   Добавьте к классу Car из предыдущего задания статический
+// //   метод getSpecs, который получает объект-машину как аргумент
+// //   и выводит в консоль значения полей maxSpeed, running и distance.
 
-//   Использование будет выглядеть следующим образом:
+// //   Использование будет выглядеть следующим образом:
 
-const someCar = new Car(100);
-someCar.turnOn();
-someCar.drive(2);
+// const someCar = new Car(100);
+// someCar.turnOn();
+// someCar.drive(2);
 
-Car.getSpecs(someCar); // maxSpeed: 100, running: true, distance: 200
+// Car.getSpecs(someCar); // maxSpeed: 100, running: true, distance: 200
 
-// */
+//Task - 05
 
-//TASK 05
-
-// /*
-//   Добавьте классу Car свойство value - цена автомобиля.
-
-//   Добавьте классу Car использование геттеров и сеттеров для свойства value.
-
-//   Геттер вернет текущей значение поля this._value
-//   Сеттер запишет в поле this._value то что ему присвоят
-
-//   PS: имя геттера и сеттера не может совпадать с полем, поэтому используйте
-//     не this.value а this._value
-
-//   Использование выглядит следующим образом:
-
-//   const myCar = new Car(50, 2000);
-
-//   myCar.value; // 2000
-//   myCar.value = 4000;
-//   myCar.value; // 4000
-// */
+/*
+  Добавьте классу Car свойство value - цена автомобиля.
+  
+  Добавьте классу Car использование геттеров и сеттеров для свойства value.
+  
+  Геттер вернет текущей значение поля this._value
+  Сеттер запишет в поле this._value то что ему присвоят
+  
+  PS: имя геттера и сеттера не может совпадать с полем, поэтому используйте
+    не this.value а this._value
+    
+  Использование выглядит следующим образом:
+  
+  const myCar = new Car(50, 2000);
+  
+  myCar.value; // 2000
+  myCar.value = 4000;
+  myCar.value; // 4000
+*/
 
 // class Car {
 //   constructor(maxSpeed, value) {
 //     // ... код
 //     this._value = value;
 //   }
+//   get value () {
+//     return this._value;
+//   }
+
+//   set value (value) {
+//     this._value = value;
+//   }
 //   // ... код
 // }
 
+class Car {
+  constructor(maxSpeed, value) {
+      //constructor( maxSpeed) {
+      /*
+        Добавьте свойства:
+          - speed - для отслеживания текущей скорости, изначально 0.
+         
+          - maxSpeed - для хранения максимальной скорости 
+          
+          - running - для отслеживания заведен ли автомобиль, 
+            возможные значения true или false. Изначально false.
+            
+          - distance - содержит общий киллометраж, изначально с 0
+      */
+      this.speed = 0;
+      this.maxSpeed = maxSpeed;
+      this.running = false;
+      this.distance = 0;
+      this._value = value;
+  }
+
+  get value() {
+      console.log(this._value);
+  }
+
+  set value(value) {
+      this._value = value;
+  }
+
+  turnOn() {
+      // Добавьте код для того чтобы завести автомобиль
+      // Просто записывает в свойство running значание true
+      return this.running = true;
+  };
+
+  turnOff() {
+      // Добавьте код для того чтобы заглушить автомобиль
+      // Просто записывает в свойство running значание false
+      return this.running = false;
+  };
+
+  accelerate(spd) {
+      // Записывает в поле speed полученное значение, при условии что
+      // оно не больше чем значение свойства maxSpeed
+      if (spd < this.maxSpeed)
+          return this.speed = spd;
+      //this.speed = spd < maxSpeed ? spd : continue;
+  };
+
+  decelerate(spd) {
+      // Записывает в поле speed полученное значение, при условии что
+      // оно не больше чем значение свойства maxSpeed и не меньше нуля
+      if (0 <= spd <= this.maxSpeed)
+          return this.speed = spd;
+      //this.speed = (0 <= spd < maxSpeed) ? spd : continue;
+  }
+
+  drive(hours) {
+      // Добавляет в поле distance киллометраж (hours умноженное на значение поля speed),
+      // но только в том случае если машина заведена!
+      if (this.running) {
+          return this.distance = hours * this.speed;
+
+      }
+  }
+}
+
+
+const myCar = new Car(50, 2000);
+
+myCar.value; // 2000
+myCar.value = 4000;
+myCar.value; // 4000
