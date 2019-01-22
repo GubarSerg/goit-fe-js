e101937dca4437fc9c0d7e92b6004c85c91987bf
GubarSerg <gubar.serg@gmail.com>
2019-01-22 02:35:44 +0200

    added 7-th task

--- modul-08/Add_tasks_08/task-07/js/add_tasks.js
+++ modul-08/Add_tasks_08/task-07/js/add_tasks.js
@@ -1,45 +1,24 @@
 "use strict";
-//Task - 07
 
 /*
-  В HTML-документе уже есть тег с id="root" (вкладка HTML)
+  Дан набор инпутов. Сделайте так, чтобы при потере фокуса все 
+  инпуты проверяли свое содержимое на правильное количество символов. 
   
-  Создайте функцию createBoxes(num), которая принимает 1 параметр num - число.
-  
-  Функция создает столько div, сколько указано в num и возвращает их в одном
-  общем контейнере. После чего необходимо повесить результат работы функции
-  в div с id="#root"
-  
-  Каждый div:
-    - Имеет случайный rgb цвет фона
-    - Размеры самого первого div - 30px на 30px.
-    - Каждый следующий div после первого, должен быть шире и выше предыдущего
-      на 10px
+  - Сколько символов должно быть в инпуте, указывается в атрибуте data-length. 
+  - Если введено подходящее количество, то outline инпута становится зеленым, 
+    если неправильное - красным. Для добавления стилей, на вкладке CSS есть стили valid и invalid
 */
 
-function createBoxes(num) {
-
-	const root = document.querySelector('#root');
-	const boxes = [];
-
-	function getRandomRgb() {
-		const x = Math.floor(Math.random() * 256);
-		const y = Math.floor(Math.random() * 256);
-		const z = Math.floor(Math.random() * 256);
-		const bgColor = "rgb(" + x + "," + y + "," + z + ")";
-		return bgColor;
-	}
-
-	for(let i = 0; i < num; i++){
-		const box = document.createElement('div');
-		
-		box.style.background = getRandomRgb();
-		box.style.width = (30 + 10 * i) +'px';
-		box.style.height = (30 + 10 * i) +'px';
-		box.textContent = 'BOX '+ (i + 1);
-		boxes.push(box)
-	}
-	return root.append(...boxes); 
+const inputs = document.querySelector('.input-list');
+inputs.addEventListener('blur', lostFocus, true);
+function lostFocus({target}) {
+	const inputDataLength = +target.dataset.length;
+	const correctLength = target.value.length;
+	if (inputDataLength === correctLength) {
+	  target.classList.add('valid'); 
+	  target.classList.remove('invalid');
+	} else {
+		target.classList.add('invalid');
+		target.classList.remove('valid');
+	};
 }
-
-createBoxes(5)
