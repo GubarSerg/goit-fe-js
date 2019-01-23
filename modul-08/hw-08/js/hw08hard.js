2cdc5305b0ce33f76800eaa9e42b8de4fa6198be
GubarSerg <gubar.serg@gmail.com>
2019-01-23 21:17:00 +0200

    addHW-08

---
+++ modul-08/hw-08/js/hw08hard.js
@@ -0,0 +1,103 @@
+'use strict'
+
+/*
+  ⚠️ ЗАДАНИЕ ПОВЫШЕННОЙ СЛОЖНОСТИ - ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
+  
+  Создайте плагин галлереи используя ES6 класс. Добавьте поля и методы класса так, 
+  чтобы можно было создать любое количество галлерей на странице. Функционал плагина 
+  аналогичный заданию выше.
+  
+  При создании экземпляра конструктор получает:
+    - items - список элементов для preview
+    - parentNode - ссылку на DOM-узел в который будут помещены fullview и preview
+    - defaultActiveItem - номер активного элемента preview по умолчанию
+    
+  Тогда создание экземпляра будет выглядеть следующим образом.
+*/
+
+class Gallery {
+    constructor({
+        items,
+        parentNode,
+        defaultActiveItem
+    }) {
+        this.items = items;
+        this.parentNode = parentNode;
+        this.defaultActiveItem = defaultActiveItem;
+        this.gallery = this.createPreviewGallery();
+        this.fullviewImg = this.createFullview();
+    }
+
+    createGallery() {
+        this.gallery.addEventListener('click', this.fullView.bind(this));
+        this.parentNode.append(this.fullviewImg, this.gallery);
+    }
+
+    createPreviewImg({
+        preview = "",
+        fullview = "",
+        alt = "",
+    }) {
+        const listItem = document.createElement('li');
+
+        const image = document.createElement('img');
+        image.setAttribute("src", preview);
+        image.setAttribute("data-fullview", fullview);
+        image.setAttribute("alt", alt);
+
+        listItem.appendChild(image);
+        return listItem;
+    }
+
+    createPreviewGallery() {
+        const list = document.createElement('ul');
+        list.classList.add('preview');
+        const previewItems = this.createPreview(this.items);
+        list.append(...previewItems);
+        return list;
+    }
+    createPreview(items) {
+        return items.map(item => this.createPreviewImg(item));
+    }
+
+    createFullviewImg({
+        fullview = "",
+        alt = "",
+      }) {
+        const fullImg = document.createElement('img');
+        fullImg.setAttribute('src', fullview);
+        fullImg.setAttribute('alt', alt);
+        return fullImg;
+      }
+
+      createFullview() {
+        const fullview = document.createElement('div');
+        fullview.classList.add('fullview');
+        fullview.appendChild(this.createFullviewImg(this.items[this.defaultActiveItem]));
+        return fullview;
+      }
+
+      fullView({
+        target
+      }) {
+        if (target.nodeName !== 'IMG') return;
+        this.fullviewImg.firstElementChild.src = target.dataset.fullview;
+        this.fullviewImg.firstElementChild.alt = target.alt;
+      }
+}
+
+const newGallery = new Gallery({
+    items: galleryItems,
+    parentNode: document.querySelector('.image-gallery'),
+    defaultActiveItem: 2
+});
+
+
+
+addGalleryBtn.addEventListener('click', addGallery);
+function addGallery(){
+    newGallery.createGallery();
+}
+
+// console.log(addGalleryBtn);
+/* Далее плагин работает в автономном режиме */
